7562eb8c0a8db4bb58a50ff4b859f89bfad2def5
Lee Wannacott <wannacottL@gmail.com>
2021-01-11 18:38:06 +1300

    Add css styling for page layout

--- index.php
+++ index.php
@@ -4,16 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Doodle</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+    <div class="wrapper indexPage">
+
     <div class="mainSection">
         <div class="logoContainer">
             <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
         </div>  
+
         <div class = "searchContainer">
-            <input class="searchBox" type="text" name="term" placeholder="Enter a serch term ...">
-            <input class="searchButton" type="submit" value="Search">
+            <form action="search.php" method="GET">
+                <input class="searchBox" type="text" name="term" placeholder="Enter a serch term ...">
+                <input class="searchButton" type="submit" value="Search">
+            </form>
         </div>
+    </div>
     </div>
 </body>
 </html>
