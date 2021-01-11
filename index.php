7c8cd4e78e700138718d4390783a4aa5345d9b0c
Lee Wannacott <wannacottL@gmail.com>
2021-01-11 17:14:58 +1300

    initial commit for  basic search engine

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Doodle</title>
+</head>
+<body>
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+        </div>  
+        <div class = "searchContainer">
+            <input class="searchBox" type="text" name="term" placeholder="Enter a serch term ...">
+            <input class="searchButton" type="submit" value="Search">
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
