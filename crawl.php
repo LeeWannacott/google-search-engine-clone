c32a08ace41f93fafa53268df6b52e1905050f4a
Lee Wannacott <wannacottL@gmail.com>
2021-01-22 19:10:20 +1300

    Converting rel links to abs

--- crawl.php
+++ crawl.php
@@ -1,6 +1,30 @@
 <?php
 include("classes/DomDocumentParser.php");
 
+function createLink($src, $url) {
+
+	$scheme = parse_url($url)["scheme"]; // http
+	$host = parse_url($url)["host"]; // website
+	
+	if(substr($src, 0, 2) == "//") {
+		$src =  $scheme . ":" . $src;
+	}
+	else if(substr($src, 0, 1) == "/") {
+		$src = $scheme . "://" . $host . $src;
+	}
+    else if (substr($src, 0, 2) == "./") {
+        $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src , 1);
+    }
+
+    else if (substr($src, 0, 3) == "../") {
+        $src = $scheme . "://" . $host . "/" . $src;
+    }
+    else if (substr($src, 0, 5) != "https" && substr($src,0,4) != "http"){
+        $src = $scheme . "://" . $host . "/" . $src;
+    }
+	return $src;
+}
+
 function followLinks($url) {
 
 	$parser = new DomDocumentParser($url);
@@ -9,11 +33,23 @@ function followLinks($url) {
 
 	foreach($linkList as $link) {
 		$href = $link->getAttribute("href");
+
+		if(strpos($href, "#") !== false) {
+			continue;
+		}
+		else if(substr($href, 0, 11) == "javascript:") {
+			continue;
+		}
+
+
+		$href = createLink($href, $url);
+
+
 		echo $href . "<br>";
 	}
 
 }
 
-$startUrl = "https://www.otago.ac.nz/";
+$startUrl = "http://www.bbc.com";
 followLinks($startUrl);
 ?>
